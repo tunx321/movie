7d87452896e46c126472f4866cbb3dc3604efad7
tungeekx <almatx78@gmail.com>
2023-10-21 22:24:05 +0600

    log middleware

--- internal/transport/http/middleware.go
+++ internal/transport/http/middleware.go
@@ -1,10 +1,21 @@
 package http
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+)
 
 func JSONMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.With(r.Method, r.URL.Path).Info("handled request")
+
 		next.ServeHTTP(w, r)
 	})
 }
